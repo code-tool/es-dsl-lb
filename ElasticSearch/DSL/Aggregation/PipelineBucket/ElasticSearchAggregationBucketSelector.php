f6cb15caebefda9e4a123a8422eff081288d5a86
asp24 <asp24>
2016-09-23 20:51:39 +0300

    ElasticSearchAggregationBucketSelector + ElasticSearchScript

---
+++ ElasticSearch/DSL/Aggregation/PipelineBucket/ElasticSearchAggregationBucketSelector.php
@@ -0,0 +1,132 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace CodeTool\ElasticSearch\DSL\Aggregation\PipelineBucket;
+
+use CodeTool\ElasticSearch\DSL\Aggregation\ElasticSearchAggregationInterface;
+use CodeTool\ElasticSearch\ElasticSearchScript;
+
+class ElasticSearchAggregationBucketSelector implements ElasticSearchAggregationInterface
+{
+    private $format = '';
+
+    private $gapPolicy = '';
+
+    /**
+     * @var ElasticSearchScript
+     */
+    private $script;
+
+    /**
+     * @var ElasticSearchAggregationInterface[]
+     */
+    private $subAggregations = [];
+
+    /**
+     * @var string[]
+     */
+    private $meta = [];
+
+    /**
+     * @var string[]
+     */
+    private $bucketsPathsMap = [];
+
+    public function format(string $format)
+    {
+        $this->format = $format;
+
+        return $this;
+    }
+
+    public function gapPolicy(string $gapPolicy)
+    {
+        $this->gapPolicy = $gapPolicy;
+
+        return $this;
+    }
+
+    public function gapInsertZeros()
+    {
+        return $this->gapPolicy('insert_zeros');
+    }
+
+    public function gapSkip()
+    {
+        return $this->gapPolicy('skip');
+    }
+
+    public function script(ElasticSearchScript $script)
+    {
+        $this->script = $script;
+
+        return $this;
+    }
+
+    public function subAggregation(string $name, ElasticSearchAggregationInterface $subAggregation)
+    {
+        $this->subAggregations[$name] = $subAggregation;
+
+        return $this;
+    }
+
+    public function meta(array $metaData)
+    {
+        $this->meta = $metaData;
+
+        return $this;
+    }
+
+    public function bucketsPathsMap(array $bucketsPathsMap)
+    {
+        $this->bucketsPathsMap = $bucketsPathsMap;
+
+        return $this;
+    }
+
+    public function addBucketsPath(string $name, string $path)
+    {
+        $this->bucketsPathsMap[$name] = $path;
+
+        return $this;
+    }
+
+    public function toArray(): array
+    {
+        $params = [];
+
+        if ('' !== $this->format) {
+            $params['format'] = $this->format;
+        }
+
+        if ('' !== $this->gapPolicy) {
+            $params['gap_policy'] = $this->gapPolicy;
+        }
+
+        if (null !== $this->script) {
+            $params['script'] = $this->script->toArray();
+        }
+
+        if ([] !== $this->bucketsPathsMap) {
+            $params['buckets_path'] = $this->bucketsPathsMap;
+        }
+
+        $result['bucket_selector'] = $params;
+
+        if (0 !== count($this->subAggregations)) {
+            $result['aggregations'] = array_map(
+                function (ElasticSearchAggregationInterface $searchAggregation) {
+                    return $searchAggregation->toArray();
+                },
+                $this->subAggregations
+            );
+        }
+
+        if ([] !== $this->meta) {
+            $result['meta'] = $this->meta;
+        }
+
+        return $result;
+    }
+}
---
+++ ElasticSearch/ElasticSearchScript.php
@@ -0,0 +1,75 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace CodeTool\ElasticSearch;
+
+use CodeTool\ElasticSearch\DSL\ElasticSearchQueryInterface;
+
+class ElasticSearchScript implements ElasticSearchQueryInterface
+{
+    private $script = '';
+
+    private $type = '';
+
+    private $lang = '';
+
+    private $params = [];
+
+    public function __construct(string $script)
+    {
+        $this->script = $script;
+    }
+
+    public function script(string $script)
+    {
+        $this->script = $script;
+
+        return $this;
+    }
+
+    public function type(string $type)
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function lang(string $lang)
+    {
+        $this->lang = $lang;
+
+        return $this;
+    }
+
+    public function param(string $name, $value)
+    {
+        $this->params[$name] = $value;
+
+        return $this;
+    }
+
+    public function toArray(): array
+    {
+        if ('' === $this->type && '' === $this->lang && [] === $this->params) {
+            return $this->script;
+        }
+
+        $result = [];
+        if ('' === $this->type) {
+            $result['inline'] = $this->script;
+        } else {
+            $result[$this->type] = $this->script;
+        }
+
+        if ('' !== $this->lang) {
+            $result['lang'] = $this->lang;
+        }
+
+        if ([] === $this->params) {
+            $result['params'] = $this->params;
+        }
+
+        return $result;
+    }
+}
